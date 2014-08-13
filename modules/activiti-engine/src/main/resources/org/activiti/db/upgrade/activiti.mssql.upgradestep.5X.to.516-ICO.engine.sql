7aeb03f63317c1738831b1e46c9495cdb8eef56a
Mike Dias <mike.rodrigues.dias@gmail.com>
2014-08-13 16:05:06 -0300

    [ico] Upgrade da versão: 5.16.4-ICO

--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mssql.upgradestep.5X.to.516-ICO.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mssql.upgradestep.5X.to.516-ICO.engine.sql
@@ -1,4 +1,4 @@
 alter table ACT_RU_TASK
 	alter column FORM_KEY_ nvarchar(2000)
 	
-	update ACT_GE_PROPERTY set VALUE_ = '5.16.3-ICO' where NAME_ = 'schema.version';
+	update ACT_GE_PROPERTY set VALUE_ = '5.16.4-ICO' where NAME_ = 'schema.version';
--- modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mysql55.upgradestep.5X.to.516-ICO.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mysql55.upgradestep.5X.to.516-ICO.engine.sql
@@ -1,4 +1,4 @@
 alter table ACT_RU_TASK
 	modify FORM_KEY_ varchar(2000);
 	
-	update ACT_GE_PROPERTY set VALUE_ = '5.16.3-ICO' where NAME_ = 'schema.version';
+	update ACT_GE_PROPERTY set VALUE_ = '5.16.4-ICO' where NAME_ = 'schema.version';
